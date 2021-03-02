8cd7cde3eae2dd77865edaedf658d893f27e67ca
Hector Fernandez <hkfernandezdev@gmail.com>
2021-03-01 18:09:38 -0600

    Add links for CDN's t the service worker cache

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -7,7 +7,9 @@ const FILES_TO_CACHE = [
   "./assets/css/styles.css",
   "./manifest.webmanifest",
   "./assets/images/icons/icon-192x192.png",
-  "./assets/images/icons/icon-512x512.png"
+  "./assets/images/icons/icon-512x512.png",
+  "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
+  "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
 ];
 
 // install
